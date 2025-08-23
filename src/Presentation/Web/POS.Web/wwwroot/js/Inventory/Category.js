61c29e8b551607b71509c14f5ef231cd800c77e2
Ashish Bhandari <ashish7bhandari@gmail.com>
2025-08-23 23:01:54 +0545

    delete confirmation WIP with error to  look after

--- src/Presentation/Web/POS.Web/wwwroot/js/Inventory/Category.js
+++ src/Presentation/Web/POS.Web/wwwroot/js/Inventory/Category.js
@@ -1,22 +1,55 @@
-﻿$(document).ready(function () {
-    $('.categoryDelete').on('click', function () {
-        const categoryId = $(this).data('category-id');
-        $.ajax({
-            url: `/Inventory/Category/Delete/${categoryId}`,
-            type: 'DELETE',
-            success: function (response) {
-                debugger;
-                //if (response.success) {
-                //    $('#bootstrap-confirm-dialog').modal('hide');
-                //    alertMessage(response.message, statusEnum.success);
-                //} else {
-                //    $('#bootstrap-confirm-dialog').modal('hide');
-                //    alertMessage(response.errors.join('\r\n'));
-                //}
+﻿
+$(function () {
+    $(".categoryDelete").each(function () {
+        const $this = $(this);
+        $this.bootstrap_confirm_delete({
+            heading: 'Confirm Delete',
+            message: " Are you sure you want to delete ?",
+            success: function (e) {
+                const categoryId = $(this).data('category-id');
+                $.ajax({
+                    url: `/Inventory/Category/Delete/${categoryId}`,
+                    type: 'DELETE',
+                    success: function (response) {
+                        if (response.success) {
+                            $('#bootstrap-confirm-dialog').modal('hide');
+                            alertMessage(response.message, statusEnum.success);
+                        } else {
+                            $('#bootstrap-confirm-dialog').modal('hide');
+                            alertMessage(response.errors.join('\r\n'));
+                        }
+                    },
+                    error: function (error) {
+                        console.log(error)
+                    }
+                })
             },
-            error: function (error) {
-                console.log(error)
+            cancel_callback: function (e) {
+                $('#bootstrap-confirm-dialog').modal('hide');
             }
-        })
-    })
+        });
+        $(document).on('click', '#bootstrap-confirm-dialog .close', function () {
+            $('#bootstrap-confirm-dialog').modal('hide')
+        });
+        //$('.categoryDelete').on('click', function () {
+        //    const categoryId = $(this).data('category-id');
+        //    $.ajax({
+        //        url: `/Inventory/Category/Delete/${categoryId}`,
+        //        type: 'DELETE',
+        //        success: function (response) {
+        //            debugger;
+        //            //if (response.success) {
+        //            //    $('#bootstrap-confirm-dialog').modal('hide');
+        //            //    alertMessage(response.message, statusEnum.success);
+        //            //} else {
+        //            //    $('#bootstrap-confirm-dialog').modal('hide');
+        //            //    alertMessage(response.errors.join('\r\n'));
+        //            //}
+        //        },
+        //        error: function (error) {
+        //            console.log(error)
+        //        }
+        //    })
+        //})
+    });
 });
