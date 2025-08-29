74daa74521252c02d50498936ed4e5bc87379329
Ashish Bhandari <ashish7bhandari@gmail.com>
2025-08-29 22:08:37 +0545

    SupplierList view created with purchaseController and supplierModule

---
+++ src/Presentation/Web/POS.Web/Controllers/PurchaseBilling/PurchaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace POS.Web.Controllers.PurchaseBilling
+{
+    [Route("[controller]")]
+    public partial class PurchaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Presentation/Web/POS.Web/Controllers/PurchaseBilling/SupplierModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POS.Web.Controllers.PurchaseBilling
+{
+    public partial class PurchaseController 
+    {
+        public IActionResult SupplierList()
+        {
+            return View();
+        }
+    }
+}
