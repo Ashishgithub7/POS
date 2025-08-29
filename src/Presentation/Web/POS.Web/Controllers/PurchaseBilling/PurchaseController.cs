7b48dcb602b1a9d4ee9f1e9812c52f0685d12be7
Ashish Bhandari <ashish7bhandari@gmail.com>
2025-08-29 22:31:41 +0545

    Supplier Create view and controller complete

--- src/Presentation/Web/POS.Web/Controllers/PurchaseBilling/PurchaseController.cs
+++ src/Presentation/Web/POS.Web/Controllers/PurchaseBilling/PurchaseController.cs
@@ -1,14 +1,32 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using POS.Business.Services.PurchaseBilling.Purchases;
+using POS.Business.Services.PurchaseBilling.Suppliers;
+using System.Security.Claims;
 
 namespace POS.Web.Controllers.PurchaseBilling
 {
     [Route("[controller]")]
     public partial class PurchaseController : Controller
     {
+        private readonly ISupplierService _supplierService;
+        private readonly IPurchaseService _purchaseService;
+
+        public PurchaseController(ISupplierService supplierService, IPurchaseService purchaseService)
+        {
+            _supplierService = supplierService;
+            _purchaseService = purchaseService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        private int GetUserId()
+        {
+            int.TryParse(User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value, out int userId);
+            return userId;
         }
     }
 }
--- src/Presentation/Web/POS.Web/Controllers/PurchaseBilling/SupplierModule.cs
+++ src/Presentation/Web/POS.Web/Controllers/PurchaseBilling/SupplierModule.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using POS.Common.Constants;
+using POS.Common.DTO.PurchaseBilling.Suppliers;
+using POS.Common.Enums;
+using POS.Web.Utilities;
 
 namespace POS.Web.Controllers.PurchaseBilling
 {
     public partial class PurchaseController 
     {
-        public IActionResult SupplierList()
+        [HttpGet("Supplier/List")]
+        public async Task<IActionResult> SupplierList()
+        {
+            var result = await _supplierService.GetAllAsync();
+            var suppliers = result.Data;
+            if (result.Status == Status.Failed) 
+            {
+                TempData[Others.ErrorMessage] = result.Error;
+            }
+            return View();
+        }
+
+        [HttpGet("Supplier/Create")]
+        public IActionResult SupplierCreate()
         {
             return View();
+        }
+
+        [HttpPost("Supplier/Create")]
+        public async Task<IActionResult> SupplierCreate(SupplierCreateDto request) 
+        {
+            request.CreatedBy = GetUserId();
+            var result = await _supplierService.SaveAsync(request);
+            if (result.Status == Status.Success)
+            {
+                TempData[Others.SuccessMessage] = result.Message;
+                return RedirectToAction("SupplierList");
+            }
+            TempData[Others.ErrorMessage] = MessageAlert.FailureAlert(result, this.ModelState);
+            return View(request);
         }
     }
 }
