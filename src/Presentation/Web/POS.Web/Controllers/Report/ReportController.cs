f577eac971b2ba81113806db76dfbc45488756cf
Ashish Bhandari <ashish7bhandari@gmail.com>
2025-08-31 20:23:45 +0545

    salesReport completed

---
+++ src/Presentation/Web/POS.Web/Controllers/Report/ReportController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using POS.Business.Services.Reporting.Sale;
+using POS.Common.Constants;
+using POS.Common.Enums;
+
+namespace POS.Web.Controllers.Report
+{
+    [Route("[controller]")]
+    [Authorize(Policy =Policy.SalesEntry)]
+    public class ReportController : Controller
+    {
+        private readonly ISalesReportService _salesReportService;
+
+        public ReportController(ISalesReportService salesReportService)
+        {
+            _salesReportService = salesReportService;
+        }
+
+        public IActionResult Sales()
+        {
+            LoadReportTypes();
+            return View();
+        }
+
+        [HttpGet("SalesReport")]
+        public async Task<IActionResult> GetSalesReport(string request) 
+        {
+            var reportType = Enum.Parse<ReportType>(request);
+            var result = await _salesReportService.GetSalesReport(reportType);
+            return Json(result.Data);        
+        }
+        private void LoadReportTypes() 
+        {
+            var result = _salesReportService.GetReportType();
+            if (result.Status == Status.Success)
+            {
+                var reportTypes = result
+                               .Data
+                               .Select(x => new SelectListItem
+                               {
+                                   Value = x,
+                                   Text = x
+                               }).ToList();
+                reportTypes.Insert(0, new SelectListItem
+                {
+                    Value = "0",
+                    Text = "Please select a Type"
+                });
+                ViewBag.ReportType = reportTypes;
+                return;
+            }
+        }
+    }
+}
---
+++ src/Presentation/Web/POS.Web/wwwroot/js/Report/Sales.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    let report = [];
+
+    $('#reportType').on('change', function () {
+        let reportType = $(this).val();
+
+        $.ajax({
+            url: `/Report/SalesReport`,
+            type: 'GET',
+            data: { request: reportType },
+            success: function (response) {
+                    report = response;
+                    loadReportTable();
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        });
+    });
+
+    function loadReportTable() {
+        let html = '';
+        html += `<tr>
+                    <td>${report[0].totalGrossAmount.toLocaleString('en-IN') }</td>
+                    <td>${report[0].totalDiscountAmount.toLocaleString('en-IN') }</td>
+                    <td>${report[0].totalDiscountPercentage.toLocaleString('en-IN') }</td>
+                    <td>${report[0].totalNetAmount.toLocaleString('en-IN') }</td>
+                    <td>${report[0].totalRecords.toLocaleString('en-IN') }</td>
+                </tr>`;
+
+        $('#salesReportBody').html(html);
+    }
+});
